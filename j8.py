4a857b63d72bbaa5ca52d665464afdeb59b23f83
ojjuiceman <okerajohnson@gmail.com>
2016-08-12 13:55:43 -0400

    flask_app.py
    This file should be names flask_app.py it is a flask app configured to run on a pythonanywhere.com server
    
    This file is pretty simple it takes the input from the user on the html file and generates a password 
    that allows the user to choose how many special characters, digits, uppercase, and lowercase letters they want
    and then proceeds to email and save the password to the database for the user. 
    
    Going to add a forgot password option next
    
    Environment- Python 2.7 Latest versions of flask on Pythonanywhere.com that their server allows

--- j8.py
+++ j8.py
@@ -8,46 +8,43 @@ from flask import render_template
 from flask_mail import Mail
 from flask_mail import Message
 
-
+#mailserver info
 MAIL_SERVER='smtp.gmail.com'
 MAIL_PORT=465
 MAIL_USE_TLS = False
 MAIL_USE_SSL= True
-MAIL_USERNAME = 'name@gmail.com'
-MAIL_PASSWORD = '**'
-
+MAIL_USERNAME = '*Your email*'
+MAIL_PASSWORD = '*Your passwowrd*'
 
 
 app = Flask(__name__)
-mail = Mail(app)
 app.config.from_object(__name__)
 
 mail = Mail(app)
 
+#makes the website
 @app.route('/')
 def my_form():
-    return render_template("index.html")
+    return render_template("form-mini.html")
 
 
-
+#takes the input from the form and creates the password, emails it and saves it to the database
 @app.route('/', methods=['POST'])
 
 def my_form_post():
-
+    #These 4 variables get the input from the form 
     digits=int(request.form['digits'])
     upper=int(request.form['uppercase'])
     lower=int(request.form['lowercase'])
     spchars=int(request.form['spcars'])
-    """ The 4 values above take the input from the forms on the website
-        The 3 values below setup the lower case letter and upeprcase letters
-    """
-
-
+   
+   
+    #These 3 variables set the letters sizes
     lowercase = string.ascii_lowercase.translate(None, "o")
     uppercase = string.ascii_uppercase.translate(None, "O")
     letters = "{0:s}{1:s}".format(lowercase, uppercase)
-    
-    #This creates the password based off all of the inputs from the user
+
+    #This chooses from the characters from the password randomly based on the variables above
     password = list(
         chain(
             (choice(uppercase) for _ in range(upper)),
@@ -57,24 +54,26 @@ def my_form_post():
             (choice(letters) for _ in range((digits - upper - lower - spchars)))
         )
     )
-    # creates a variable for the users email
+    # These 3 variables help produce the outputs and messages later int he file
     damail = str(request.form['email'])
-    # creates a variable for the password
+
     dapass = "".join(sample(password, len(password)))
-    # creates and sends the email to the user
+
+    lstoutput = (' Hello ') + damail + ('  your password is   ') + dapass
+    #Creates the email
     msg = Message(
        'Hello',
-       sender='namen@gmail.com',
+       sender='passcreator',
        recipients= [str(damail)])
     msg.body = "Hello thanks" + (' your password is  ') + dapass
     mail.send(msg)
-    # opens and writes to the text database file
-    with open('*yourappsdirectory*\database.txt','a') as fo:
+    # opens the database file and saves the password email and employee id (as stid)
+    with open('/home/zryder/mysite/static/database.txt','a') as fo:
         fo.write(str(request.form['email']) + (' ') + str(request.form['stid']) + (' ') + dapass)
-    
-    # returns a message with the users password
-    return str(request.form['email']) + ('  your password is   ') + dapass
 
-# Runs the webapp      
+    #prints the output from the variable above on the next webpage
+    return render_template("index.html", lstoutput=lstoutput)
+
+
 if __name__ == '__main__':
     app.run()
